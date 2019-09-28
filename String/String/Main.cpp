afde6873160c4d09e8e45bef012d8a2866e8a1c3
ArtemGord1728 <artemseraf01@mail.ru>
2019-09-28 20:08:42 +0300

    added operator <<

--- String/String/Main.cpp
+++ String/String/Main.cpp
@@ -5,40 +5,37 @@ using namespace std;
 class String
 {
 public:
-	char *str;
-	int length;
+	char *string_buffer;
+	unsigned int size_buffer;
 
 	String()
 	{
-		str = nullptr;
-		length = 0;
+		string_buffer = nullptr;
+		size_buffer = 0;
 	}
 
-
-	String(char *str)
+	String(const char* str)
 	{
-		length = strlen(str);
-		this->str = new char[length + 1];
+		size_buffer = strlen(str);
+		this->string_buffer = new char[size_buffer + 1];
 
-		for (int i = 0; i < length; i++)
+		for (int i = 0; i < size_buffer; i++)
 		{
-			this->str[i] = str[i];
+			string_buffer[i] = str[i];
 		}
-		this->str[length] = '\0';
+		string_buffer[size_buffer] = '\0';
 	}
 
 	String(const String &val_1)
 	{
+		size_buffer = strlen(val_1.string_buffer);
+		this->string_buffer = new char[size_buffer + 1];
 
-
-		length = strlen(val_1.str);
-		this->str = new char[length + 1];
-
-		for (int i = 0; i < length; i++)
+		for (int i = 0; i < size_buffer; i++)
 		{
-			this->str[i] = val_1.str[i];
+			this->string_buffer[i] = val_1.string_buffer[i];
 		}
-		this->str[length] = '\0';
+		this->string_buffer[size_buffer] = '\0';
 
 	}
 
@@ -46,63 +43,68 @@ public:
 
 	String& operator=(const String &val)
 	{
-		if (this->str != nullptr)
+		if (this->string_buffer != nullptr)
 		{
-			delete[] str;
+			delete[] string_buffer;
 		}
 
 
-		length = strlen(val.str);
-		this->str = new char[length + 1];
+		size_buffer = strlen(val.string_buffer);
+		this->string_buffer = new char[size_buffer + 1];
 
-		for (int i = 0; i < length; i++)
+		for (int i = 0; i < size_buffer; i++)
 		{
-			this->str[i] = val.str[i];
+			this->string_buffer[i] = val.string_buffer[i];
 		}
-		this->str[length] = '\0';
+		this->string_buffer[size_buffer] = '\0';
 
 		return *this;
+	}
+
+	friend std::ostream& operator<< (std::ostream& stream, const String& str) {
+		stream << str.string_buffer;
+		return stream;
 	}
 
 	String& operator+(const String &obj_1)
 	{
 		String newStr;
-		int this_Length = strlen(str);
-		int other_Length = strlen(obj_1.str);
+		int this_Length = strlen(string_buffer);
+		int other_Length = strlen(obj_1.string_buffer);
 
-		newStr.str = new char[this_Length + other_Length + 1]; // хранится динам. массив с длинами строк и в конце добавляется детерминирующий ноль
+		newStr.string_buffer = new char[this_Length + other_Length + 1]; // хранится динам. массив с длинами строк и в конце добавляется детерминирующий ноль
 
 		int i = 0;
 		for (; i < this_Length; i++)
 		{
-			newStr.str[i] = this->str[i];
+			newStr.string_buffer[i] = this->string_buffer[i];
 		}
 
 		int j = 0;
 		for (; j < other_Length; j++, i++)
 		{
-			newStr.str[i] = obj_1.str[j];
+			newStr.string_buffer[i] = obj_1.string_buffer[j];
 		}
 
-		newStr.str[this_Length + other_Length] = '\0';
+		newStr.string_buffer[this_Length + other_Length] = '\0';
 		return newStr;
 
 	}
 
 	bool operator ==(const String &obj)
 	{
-		return this->length == obj.length;
+		return this->size_buffer == obj.size_buffer;
 	}
 
 	bool operator!=(const String &obj)
 	{
-		return this->length != obj.length;
+		return this->size_buffer != obj.size_buffer;
 	}
 
 
 	~String()
 	{
-		delete[] this->str;
+		delete[] this->string_buffer;
 	}
 };
 
@@ -112,14 +114,10 @@ int main()
 {
 	setlocale(0, "ru");
 
-	char g[] = "Hello";
-	char q[] = "Hello";
-	String test_1(g);
-	String test_2(q);
+	String test_1 = "hello";
+	String test_2 = " world";
 
-	bool res = test_1 != test_2;
-
-	cout << res << endl;
+	cout << test_1 << test_2 << endl;
 
 	system("pause");
 	return 0;
